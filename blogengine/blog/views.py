0cd6b2dfec8685a02c0a645ad2064b3ddfe3013e
BrunerDmitry <bruner.expert@yandex.ru>
2023-04-26 07:36:11 +0300

    были созданы представления классы на основе (View) вместо функций. использован метод get для получения информации о запросе и выдачи данных. после get в обоих функциях был заменен на миксин для соблюдения принципа dry.

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import get_object_or_404
+
 from .models import *
+from .utils import *
+
 
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', {'posts': posts})
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/list_posts_tag.html', context={'tag': tag})
+
+class PostDetail(ObjectDatailMixin, View): # порядок наследования важен! сначала ищется в первом классе и если не находит то во втором. для вывода иерархии обратиться к методу класса mro()
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagDetail(ObjectDatailMixin, View):
+    model = Tag
+    template = 'blog/list_posts_tag.html'
