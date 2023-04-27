894bcf35d634482a281a6432ffcb76e675fc7a6e
BrunerDmitry <bruner.expert@yandex.ru>
2023-04-27 22:14:54 +0300

    создали миксин для генерации постов и тегов чтобы код был dry. также сделали генерацию уникального слага на основе функций time and slugify

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -1,15 +1,28 @@
 from django.db import models
 from django.shortcuts import reverse
 
+from django.utils.text import slugify
+from time import time
+
+def gen_slug(s):
+    new_slug = slugify(s, allow_unicode=True)
+    return new_slug + '-' + str(int(time()))
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, blank=True, unique=True)
     body = models.TextField(blank=True, db_index=True)
     date_pub = models.DateTimeField(auto_now_add=True)
-    tags = models.ManyToManyField('Tag', blank=True, related_name='posts') # рекомендуется писать в главном классе а не в обслуживающем это не конвенция а просто мнение  OM
+    tags = models.ManyToManyField('Tag', blank=True,
+                                  related_name='posts')  # рекомендуется писать в главном классе а не в обслуживающем это не конвенция а просто мнение  OM
 
-    def get_absolute_url(self): #создает ссылку на объект тем самым упрощая доступ по соглашениям джанго в разных местах кода
-        return reverse('post_detail_url', kwargs={'slug': self.slug}) #функция формирует ссылку на конкретный объект
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
+    def get_absolute_url(
+            self):  # создает ссылку на объект тем самым упрощая доступ по соглашениям джанго в разных местах кода
+        return reverse('post_detail_url', kwargs={'slug': self.slug})  # функция формирует ссылку на конкретный объект
+
     def __str__(self):
         return self.title
 
--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -28,31 +28,24 @@ class TagDetail(ObjectDatailMixin, View):
     template = 'blog/list_posts_tag.html'
 
 
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm
-        return render(request, 'blog/tag_create.html', context={'form': form})
+class TagCreate(ObjectCreateMixin, View):
+    form_model = TagForm
+    template = 'blog/tag_create.html'
+    #
+    # def get(self, request):
+    #     form = TagForm
+    #     return render(request, 'blog/tag_create.html', context={'form': form})
+    #
+    # def post(self, request):
+    #     bound_form = TagForm(request.POST)
+    #
+    #     if bound_form.is_valid():
+    #         new_tag = bound_form.save()
+    #         return redirect(new_tag)
+    #     else:
+    #         return render(request, 'blog/tag_create.html', context={'form': bound_form})
+    #
 
-    def post(self, request):
-        bound_form = TagForm(request.POST)
-
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        else:
-            return render(request, 'blog/tag_create.html', context={'form': bound_form})
-
-
-class PostCreate(View):
-    def get(self, request):
-        form = PostForm
-        return render(request, 'blog/post_create_form.html', context={'form': form})
-
-    def post(self, request):
-        bound_form = PostForm(request.POST)
-
-        if bound_form.is_valid():
-            new_post = bound_form.save()
-            return redirect(new_post)
-        else:
-            return render(request, 'blog/post_create.html', context={'form': bound_form})
+class PostCreate(ObjectCreateMixin, View):
+    form_model = PostForm
+    template = 'blog/post_create_form.html'
